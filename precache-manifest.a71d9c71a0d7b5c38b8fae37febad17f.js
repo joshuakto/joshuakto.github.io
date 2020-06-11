c36167f2444124649d24a576c76ed240e2704905
joshuakto <joshuakto@gmail.com>
2020-06-11 19:53:13 +0800

    Updates

---
+++ precache-manifest.a71d9c71a0d7b5c38b8fae37febad17f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7e8cba44df4a5ce8b476b8d48ce40f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e4c90f559cba22df066",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc4cd291fce314c7a707",
+    "url": "/static/js/2.f4a83b0a.chunk.js"
+  },
+  {
+    "revision": "46f0aed71940a39cfa9ad07335db7264",
+    "url": "/static/js/2.f4a83b0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4c90f559cba22df066",
+    "url": "/static/js/main.df92f9f9.chunk.js"
+  },
+  {
+    "revision": "24e8d5a84308de897e3a",
+    "url": "/static/js/runtime-main.c8f0954f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
